7cf2432bd6584133773189b8128fff6a7f7fa88c
Khoi Nguyen <khoi@nguyen.me.uk>
2023-04-05 16:11:24 +0200

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -gx EDITOR nvim
+
+abbr -a -- tn 'tmux new -s'
+abbr -a -- vim nvim
+abbr -a -- g git
+abbr -a -- ta
+abbr -a -- gs 'git status'
